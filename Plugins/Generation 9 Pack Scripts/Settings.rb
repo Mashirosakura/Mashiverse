2fd2540ec587149c9d468cb15afb31989bd7b481
Mashirosakura <72957526+Mashirosakura@users.noreply.github.com>
2026-04-14 14:42:13 +0100

    Update 14/04/26 #2
    Added more moves to the PBS and updated Gen 9 Pack to have Snow over Hail

--- Plugins/Generation 9 Pack Scripts/Settings.rb
+++ Plugins/Generation 9 Pack Scripts/Settings.rb
@@ -28,7 +28,7 @@ module Settings
   # treated as separate status conditions.
   #-----------------------------------------------------------------------------
   SLEEP_EFFECTS_CAUSE_DROWSY     = false
-  FREEZE_EFFECTS_CAUSE_FROSTBITE = false
+  FREEZE_EFFECTS_CAUSE_FROSTBITE = true
   #-----------------------------------------------------------------------------
   # When true, Sleep can be cured by getting hit from electrocute moves.
   # (Spark, Volt Tackle, and Wild Charge)
